c6c5f339486693e2f158aaa9651db299a6a300f2
MarkBarlescu <barlescu92@gmail.com>
2019-04-07 16:23:57 -0700

    added css to match

--- src/App.js
+++ src/App.js
@@ -24,14 +24,25 @@ class App extends Component {
           
           <hr />
         </header>
+
         <div>
           <p>Prepare for your career with a Project Management, Web-Development, Graphic design, or Digital Marketing Internship at Northern.</p>
+          <p id='hidden' hidden>Please enter a valid email address.</p>
           <form onSubmit={this.submit}>
-            <p id='hidden' hidden>dude</p>
-            <input type='text' id='email' ref='email' placeholder='Your Email Address' />
-            <input type='text' placeholder='Your Interests' />
-            <button>Sign Up Now</button>
-            </form>
+            <div class='innerForm'>
+              <div class='inputs'>
+                <input type='text' id='email' ref='email' placeholder='Your Email Address' />
+                <select name='interests' id='interests'>
+              
+                  <option value="" disabled selected hidden>Your Interests</option>
+                  <option value='Development'>Development</option>
+                </select>
+              </div>
+              <div class='button'>
+                <button>Sign Up Now</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     );
